4d99870844f3880b42ce78c7ec99acf50e077ee6
CodingBeagle <kasper.rie@gmail.com>
2023-06-29 13:58:05 +0200

    start vulkan initialization

--- application/src/main.rs
+++ application/src/main.rs
@@ -1,10 +1,33 @@
-use glfw::{self, glfwInit};
+// CString represents an owned, C-compatible, nul-terminated string with no nul bytes in the middle.
+use std::ffi::CString;
+use std::ptr;
+
+// Internal Crates
+use glfw::{self, glfwInit, glfwVulkanSupported, glfwWindowHint, GLFW_CLIENT_API, GLFW_NO_API, glfwCreateWindow};
 
 fn main() {
     unsafe {
         // Initialize GLFW
-        if glfwInit() == -1 {
-            println!("Failed to initialize GLFW!");
+        if glfwInit() != 1 {
+            panic!("Failed to initialize GLFW!")
         };
+
+        // Check for Vulkan support
+        if glfwVulkanSupported() != 1 {
+            panic!("Vulkan not supported on current platform.")
+        }
+
+        // Create a Vulkan context window
+
+        // We disable context creation because we will use Vulkan, not OpenGL.
+        glfwWindowHint(GLFW_CLIENT_API as i32, GLFW_NO_API as i32);
+
+        let window_title = CString::new("2D Rendering").expect("Failed to create CString");
+        let window_handle = glfwCreateWindow(
+            800,
+            600,
+            window_title.as_ptr(),
+            ptr::null_mut(),
+            ptr::null_mut());
     }
 }
